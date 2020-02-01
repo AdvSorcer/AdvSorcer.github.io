7bcbc3fbfda5ae30a358ccef306a5d1bd6ca3f44
蔡昀翰 <hank2382551@hotmail.com.tw>
2020-02-01 19:21:44 +0800

    add works block

---
+++ Resume/resume.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    Next()
+});
+
+let Next = () =>{
+    let ImgTledep = document.getElementById("ImgTldep")
+    let src = ImgTledep.src;
+    console.log(src)
+    let src2  = "freeteatime.com/resume/"
+}
