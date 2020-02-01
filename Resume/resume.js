8a835861b96dc47840cd83e7e9cce1786a98924c
蔡昀翰 <hank2382551@hotmail.com.tw>
2020-02-01 21:08:57 +0800

    add two button

--- Resume/resume.js
+++ Resume/resume.js
@@ -1,10 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
-    Next()
+    init()
 });
-
+let url = "works/tldep/"
+//https://freeteatime.com/Resume/works/tldep/1.png
+let number = 1;
 let Next = () =>{
     let ImgTledep = document.getElementById("ImgTldep")
-    let src = ImgTledep.src;
-    console.log(src)
-    let src2  = "freeteatime.com/resume/"
+    if(number==8){
+        number =0;
+    }
+    number +=1;
+    let src = url + number + ".png"
+    ImgTledep.src = src;
+}
+let Pre =() =>{
+    let ImgTledep = document.getElementById("ImgTldep")
+    number -=1;
+    if(number==0){
+        number =8;
+    }
+    let src = url + number + ".png"
+    ImgTledep.src = src;
+}
+let init = ()=>{
+    let btnNext = document.getElementById('btn_next')
+    btnNext.addEventListener('click',()=>{
+        Next();
+    })
+    let btnPre = document.getElementById('btn_pre')
+    btnPre.addEventListener('click',()=>{
+        Pre();
+    })
 }
